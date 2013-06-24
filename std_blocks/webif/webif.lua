6453ae6396418d69b67dcbe14df226422f23491e
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2013-06-24 10:17:34 +0200

    writes types in multicolum

--- std_blocks/webif/webif.lua
+++ std_blocks/webif/webif.lua
@@ -2,6 +2,10 @@ local ffi=require"ffi"
 local u5c=require"u5c"
 local utils=require"utils"
 
+-- Configuration
+num_type_cols=3
+
+--- define mongoose request info
 ffi.cdef[[
 struct mg_request_info {
   const char *request_method; // "GET", "POST", etc
@@ -23,7 +27,7 @@ struct mg_request_info {
 ]]
 
 
-function safe_tostring(charptr)
+function safe_ts(charptr)
    if charptr == nil then return "" end
    return ffi.string(charptr)
 end
@@ -44,6 +48,8 @@ response=[[
 </html>
 ]]
 
+function html(...) return response:format(table.concat({...}, "\n")) end
+
 function reqinf_tostr(ri)
    return ([[
 request method: %s <br>
@@ -55,88 +61,105 @@ remote_user:    %s <br>
 <br>
 generated on %s
    ]]):format(
-      safe_tostring(ri.request_method),
-      safe_tostring(ri.uri),
-      safe_tostring(ri.http_version),
-      safe_tostring(ri.query_string),
-      safe_tostring(ri.remote_user),
+      safe_ts(ri.request_method),
+      safe_ts(ri.uri),
+      safe_ts(ri.http_version),
+      safe_ts(ri.query_string),
+      safe_ts(ri.remote_user),
       os.date("%A %B %d, %Y, %X"))
 end
 
-function u5c_overview_tohtml(ni)
+--- Generate html tables of the known u5c types.
+-- @param ni node_info
+-- @return string containing html
+function typelist_tohtml(ni)
+   local table_header, table_footer, elem_per_tab
+   local entries={}
    local output={}
-   output[#output+1]=
-      [[
-<h2>Registered types</h2>
-<table border="0" cellspacing="0" cellpadding="0">
+
+   output[#output+1]="<h2>Registered types</h2>"
+
+   table_header = [[
+<table border="0" style="float:left; position:relative;" cellspacing="0" cellpadding="0">
   <tr>
    <th>name</th>
    <th>size</th>
-  </tr>
-      ]]
-
-   u5c.types_foreach(ni,
-		     function(t)
-			output[#output+1]=
-			   "<tr><td><tt>"..ffi.string(t.name).."</tt></td><td>"..tostring(t.size).."</td></tr>"
-		     end)
-
-   output[#output+1]="</table><br><br>"
-
+  </tr> ]]
    
-   
+   table_footer = '</table>'
+
+   -- generate a single table entry and append it to entries
+   local function gen_type_entry(t)
+      entries[#entries+1]=
+	 "<tr><td><tt>"..ffi.string(t.name).."</tt></td><td>"..tostring(t.size).."</td></tr>"
+   end
+
+   -- generate list of entries
+   u5c.types_foreach(ni, gen_type_entry)
+
+   -- write then out in num_type_cols tables
+   elem_per_tab = math.ceil(#entries/num_type_cols)
+
+   local cnt=0
+
+   output[#output+1]=table_header
+
+   while cnt+1 <= #entries do
+      if cnt~=0 and cnt % elem_per_tab == 0 then
+	 output[#output+1]="\n\n\n"
+	 output[#output+1]=table_footer
+	 output[#output+1]=table_header
+      end
+      output[#output+1]=entries[cnt+1]
+      cnt=cnt+1
+   end
+
+   output[#output+1]=table_footer
+   output[#output+1]='<div style="clear:left"></div>'
    return table.concat(output, "\n")
 end
 
---h1 { color:red; font-size:48px; }
+
+--- Registered blocks
+function blocklist_tohtml(ni)
+   
+end
+
+
+--- Stylesheet for the u5c webinterface
 stylesheet_str=[[
 body{
-	background: #ffffff;
-	color: #000000;
-	font-size: 14px;
-	margin: 0;
-	padding: 0;
-	text-align: left;
-	font-family: arial, verdana, sans-serif;
-	font-weight:normal;
+   background-color: #CCE5F0;
+   color: #000000;
+   font-size: 14px;
+   margin: 0;
+   padding: 0;
+   text-align: left;
+   font-family: arial, verdana, sans-serif;
+   font-weight:normal;
 }
 
-h1{
-	font-size: 32px;
-	color: #000000;
-	font-weight: normal;
-	text-decoration: none;
+h1{ font-size: 32px; color: #000000; font-weight: normal; text-decoration: none; }
+h2{ font-size:24px; color:#000000; font-weight:normal; }
+a{ }
+a:hover{ }
+
+table {
+   margin: 20px;
 }
 
-h2{
-	font-size:24px;
-	color:#000000;
-	font-weight:normal;
-}
-
-a{
-}
-
-a:hover{
-}
-
-table,td,th {
-   /* border: 0px solid #000000; */
-}
-
-td{
-   padding-right:10px;
-}
+td,th { /* border: 0px solid #000000; */ }
+td{ padding-right:10px; }
 
 ]]
 
--- master dispatch table
+--- master dispatch table.
+-- This 
 dispatch_table = {
    ["/"] = function(ri, ni)
-	      return response:format(u5c_overview_tohtml(ni)..reqinf_tostr(ri))
+	      return html(typelist_tohtml(ni), reqinf_tostr(ri))
 	   end,
 
-   -- ["/reload"]=function() return "__reload__" end,
    ["/style.css"]=function() return stylesheet_str, "text/css" end,
 }
 
@@ -145,7 +168,7 @@ function request_handler(node_info, request_info_lud)
    local reqinf = ffi.cast("struct mg_request_info*", request_info_lud)
    local ni = ffi.cast("struct u5c_node_info*", node_info)
 
-   local uri = safe_tostring(reqinf.uri)
+   local uri = safe_ts(reqinf.uri)
    local handler = dispatch_table[uri]
 
    print("requesting uri", uri)
