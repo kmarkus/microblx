478a17839eb41b5258907fa294889d83c1fa8dea
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2013-08-08 11:37:41 +0200

    simplify cdata_tolua

--- lua/cdata_tolua.lua
+++ lua/cdata_tolua.lua
@@ -3,59 +3,39 @@ local reflect = require "reflect"
 
 local M={}
 
-function M.cdata_tolua(cd)
-   
-   if type(cd)~='cdata' then return cd end
-   local refct = reflect.typeof(cd)
+function M.cdata_tolua(cd, refct)
    local res
 
-   if refct.what=='struct' then
+   if type(cd)~='cdata' then return cd end
+   refct = refct or reflect.typeof(cd)
+
+   local function do_struct(cd, refct)
       res = {}
       for field_refct in refct:members() do
 	 res[field_refct.name]=M.cdata_tolua(cd[field_refct.name])
       end
-   elseif refct.what=='union' then
-      return "<union "..refct.name..">"
-   elseif refct.what=='ref' then
-      if refct.element_type.what=='struct' then
-	 -- create copy and recall ourself
-	 res=M.cdata_tolua(ffi.new('struct '..refct.element_type.name, cd))
-      elseif refct.element_type.what=='array' 
-	 -- guess what might be strings
-	 and refct.element_type.element_type.what=='int'
-	 and refct.element_type.element_type.size==1 then
-	 res=ffi.string(cd)
-      elseif refct.element_type.what=='array' then
-	 -- any other type of array
-	 local num_elem=refct.element_type.size/refct.element_type.element_type.size
-	 res={}
- 	 for i=0,num_elem-1 do res[i+1]=M.cdata_tolua(cd[i]) end
-      end
-   elseif refct.what=='ptr' then
-      -- We can't reuse the 'ref' code above for this, because it is
-      -- apparently not legal to initalize a struct from a
-      -- pointer. Hence, we create the copy and use ffi.copy to copy
-      -- the from the original
-      if refct.element_type.what=='struct' then
-	 local ct = ffi.typeof('struct '..refct.element_type.name)
-	 local copy=ct()
-	 ffi.copy(copy, cd, ffi.sizeof(ct))
-	 res=M.cdata_tolua(copy)
-      end
-   elseif refct.what=='int' or refct.what=='float' then
-      res=tonumber(cd)
-   elseif refct.what=='array' then
-      if refct.element_type.what=='int' and 
-	 refct.element_type.size==1 then
-	 res=ffi.string(cd)
-      else
-	 local num_elem=refct.size / refct.element_type.size
-	 res={}	
- 	 for i=0,num_elem-1 do res[i+1]=M.cdata_tolua(cd[i]) end
-      end
-   else
-      print("can't handle "..refct.what..", ignoring.")
+      return res
    end
+
+   local function is_string(refct)
+      if refct.element_type.what=='int' and refct.element_type.size==1 then return true end
+      return false
+   end
+
+   local function do_array(cd, refct)
+      if is_string(refct) then return ffi.string(cd) end
+      local res={}
+      local num_elem=refct.size/refct.element_type.size
+      for i=0,num_elem-1 do res[i+1]=M.cdata_tolua(cd[i]) end
+      return res
+   end
+
+   if refct.what=='int' or refct.what=='float' then res=tonumber(cd)
+   elseif refct.what=='struct' then res=do_struct(cd, refct)
+   elseif refct.what=='array' then res=do_array(cd, refct)
+   elseif refct.what=='ref' or refct.what=='ptr' then res=M.cdata_tolua(cd, refct.element_type)
+   elseif refct.what=='union' then return "<union "..refct.name..">"
+   else print("can't handle "..refct.what..", ignoring.") end
    return res
 end
 
