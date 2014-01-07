4e772439bff4bb3e0339b6ccfef05a3ea54188c8
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2014-01-07 11:58:42 +0100

    add blockdiagram composition support + example

--- lua/blockdiagram.lua
+++ lua/blockdiagram.lua
@@ -88,39 +88,69 @@ configs_spec = TableSpec
 }
 
 --- system spec
+
 system_spec = ObjectSpec
 {
    name='system',
    type=system,
    sealed='both',
    dict={
+      include = TableSpec {
+	 name='include', -- subsystems
+	 array={ }, -- self reference added below
+	 sealed='both',
+      },
       imports=imports_spec,
       blocks=blocks_spec,
       connections=connections_spec,
       configurations=configs_spec,
    },
-   optional={'imports', 'blocks', 'connections', 'configurations' },
+   optional={ 'include', 'imports', 'blocks', 'connections', 'configurations' },
 }
+
+-- add self include reference
+system_spec.dict.include.array[#system_spec.dict.include.array+1] = system_spec
+
+function is_system(s)
+   return umf.uoo_type(s) == 'instance' and umf.instance_of(system, s)
+end
 
 --- Validate a blockdiagram model.
 function system:validate(verbose)
    return umf.check(self, system_spec, verbose)
 end
 
+--- Load system from file.
+-- @param file name of file
+-- @return system
+function load(file)
+   local sys = dofile(file)
+   if not is_system(sys) then
+      error("blockdiagram.load: no valid system in file '" .. tostring(file) .. "' found.")
+   end
+   return sys
+end
 
 --- Launch a blockdiagram system
 -- @param self system specification to load
 -- @param t configuration table
 -- @return ni node_info handle
-function system:launch(t)
+function system.launch(self, t)
    if self:validate(false) > 0 then
       self:validate(true)
       os.exit(1)
    end
 
    local log
-   if t.verbose then log=print
+   local ind_log = -1
+   local ind_mul = 4
+   if t.verbose then
+      log=function(first, ...)
+	 print(string.rep(' ', ind_log*ind_mul)..tostring(first), ...)
+      end
    else log=function() end end
+
+   local imported_modules={}
 
    -- Preprocess configs
    -- Substitue #blockanme with corresponding ubx_block_t ptrs
@@ -141,115 +171,143 @@ function system:launch(t)
       return ret
    end
 
-   self.imports = self.imports or {}
-   self.blocks = self.blocks or {}
-   self.configurations = self.configurations or {}
-   self.connections = self.connections or {}
+   -- Instatiate the system recursively.
+   -- @param self system
+   -- @param t global configuration
+   -- @param ni node_info
+   local function __launch(self, t, ni)
+      ind_log=ind_log+1
 
-   local ni = ubx.node_create(t.nodename)
+      self.include = self.include or {}
+      self.imports = self.imports or {}
+      self.blocks = self.blocks or {}
+      self.configurations = self.configurations or {}
+      self.connections = self.connections or {}
 
-   log("launching block diagram system in node "..ts(t.nodename))
+      log("launching block diagram system in node "..ts(t.nodename))
 
-   log("importing "..ts(#self.imports).." modules... ")
-   utils.foreach(function(m)
-		    log("    "..magenta(m) )
-		    ubx.load_module(ni, m)
-		 end, self.imports)
-   log("importing modules completed")
+      if #self.include > 0 then
+	 log("loading used subsystems")
+	 utils.foreach(function(s) __launch(s, t, ni) end, self.include)
+	 log("loading subsystems completed")
+      end
 
-   log("instantiating "..ts(#self.blocks).." blocks... ")
-   local btab = {}
-   utils.foreach(function(b)
-		    log("    "..green(b.name).." ["..blue(b.type).."]")
-		    btab[b.name] = ubx.block_create(ni, b.type, b.name)
-		 end, self.blocks)
-   log("instantiating blocks completed")
+      if #self.imports > 0 then
+	 log("importing "..ts(#self.imports).." modules... ")
+	 utils.foreach(function(m)
+			  if imported_modules[m] then return end
+			  log("    "..magenta(m) )
+			  ubx.load_module(ni, m)
+			  imported_modules[m]=true
+		       end, self.imports)
+	 log("importing modules completed")
+      end
 
-   log("configuring "..ts(#self.configurations).." blocks... ")
-   log("    preprocessing configs")
-   if not preproc_configs(ni, self.configurations) then
-      log(red("    failed"))
-      os.exit(1)
+      if #self.blocks > 0 then
+	 log("instantiating "..ts(#self.blocks).." blocks... ")
+	 utils.foreach(function(b)
+			  log("    "..green(b.name).." ["..blue(b.type).."]")
+			  ubx.block_create(ni, b.type, b.name)
+		       end, self.blocks)
+	 log("instantiating blocks completed")
+      end
+
+      if #self.configurations > 0 then
+	 log("configuring "..ts(#self.configurations).." blocks... ")
+	 log("    preprocessing configs")
+	 if not preproc_configs(ni, self.configurations) then
+	    log(red("    failed"))
+	    os.exit(1)
+	 end
+
+	 utils.foreach(
+	    function(c)
+	       local b = ubx.block_get(ni, c.name)
+	       if b==nil then
+		  log("    no block named "..c.name.." ignoring configuration", utils.tab2str(c.config))
+	       else
+		  if type(c.config)=='string' then
+		     log("    "..green(c.name).." (from file"..yellow(c.config)..")")
+		  else
+		     log("    "..green(c.name).." with "..yellow(utils.tab2str(c.config))..")")
+		     ubx.set_config_tab(b, c.config)
+		  end
+	       end
+	    end, self.configurations)
+	 log("configuring blocks completed")
+      end
+
+      if #self.connections > 0 then
+	 log("creating "..ts(#self.connections).." connections... ")
+	 utils.foreach(function(c)
+			  local bnamesrc,pnamesrc = unpack(utils.split(c.src, "%."))
+			  local bnametgt,pnametgt = unpack(utils.split(c.tgt, "%."))
+
+			  -- are we connecting to an interaction?
+			  if pnamesrc==nil then
+			     -- src="iblock", tgt="block.port"
+			     local ib = ubx.block_get(ni, bnamesrc)
+
+			     if ib==nil then  log(red("unkown block "..bnamesrc)); os.exit(1) end
+			     if not ubx.is_iblock_instance(ib) then
+				log(red(ts(bnamesrc).." not a valid iblock instance"))
+				os.exit(1)
+			     end
+
+			     local btgt = ubx.block_get(ni, bnametgt)
+			     local ptgt = ubx.port_get(btgt, pnametgt)
+
+			     if ubx.port_connect_in(ptgt, ib) ~= 0 then
+				log(red("failed to connect interaction "..bnamesrc..
+					   " to port "..ts(bnametgt).."."..ts(pnametgt)))
+			     end
+			     log("    "..green(bnamesrc).." (iblock) -> "..green(ts(bnametgt)).."."..cyan(ts(pnametgt)))
+
+			  elseif pnametgt==nil then
+			     -- src="block.port", tgt="iblock"
+			     local ib = ubx.block_get(ni, bnametgt)
+
+			     if ib==nil then log(red("unkown block "..ts(bnametgt))); os.exit(1) end
+
+			     if not ubx.is_iblock_instance(ib) then
+				log(red(ts(bnametgt).." not a valid iblock instance"))
+				os.exit(1)
+			     end
+
+			     local bsrc = ubx.block_get(ni, bnamesrc)
+			     local psrc = ubx.port_get(bsrc, pnamesrc)
+
+			     if ubx.port_connect_out(psrc, ib) ~= 0 then
+				log(red("failed to connect "..ts(bnamesrc).."."..ts(pnamesrc)..
+					   " to "..ts(bnametgt)).." (iblock)")
+			     end
+			     log("    "..green(ts(bnamesrc)).."."..cyan(ts(pnamesrc)).." -> "..green(bnametgt).." (iblock)")
+			  else
+			     -- standard connection between two cblocks
+			     local bufflen = c.buffer_length or 1
+			     log("    "..green(ts(bnamesrc))..'.'..cyan(ts(pnamesrc)).." -["..
+				    yellow(ts(bufflen), true).."]".."-> "..green(ts(bnametgt)).."."..cyan(ts(pnametgt)))
+			     local bsrc = ubx.block_get(ni, bnamesrc)
+			     local btgt = ubx.block_get(ni, bnametgt)
+			     ubx.conn_lfds_cyclic(bsrc, pnamesrc, btgt, pnametgt, bufflen)
+			  end
+		       end, self.connections)
+
+	 log("creating connections completed")
+      end
+      ind_log=ind_log-1
    end
 
-   utils.foreach(
-      function(c)
-	 if btab[c.name]==nil then
-	    log("    no block named "..c.name.." ignoring configuration", utils.tab2str(c.config))
-	 else
-	    if type(c.config)=='string' then
-	       log("    "..green(c.name).." (from file"..yellow(c.config)..")")
-	    else
-	       log("    "..green(c.name).." with "..yellow(utils.tab2str(c.config))..")")
-	       ubx.set_config_tab(btab[c.name], c.config)
-	    end
-	 end
-      end, self.configurations)
-
-   log("configuring blocks completed")
-
-   log("creating "..ts(#self.connections).." connections... ")
-   utils.foreach(function(c)
-		    local bnamesrc,pnamesrc = unpack(utils.split(c.src, "%."))
-		    local bnametgt,pnametgt = unpack(utils.split(c.tgt, "%."))
-
-		    -- are we connecting to an interaction?
-		    if pnamesrc==nil then
-		       -- src="iblock", tgt="block.port"
-		       local ib = ubx.block_get(ni, bnamesrc)
-
-		       if ib==nil then  log(red("unkown block "..bnamesrc)); os.exit(1) end
-		       if not ubx.is_iblock_instance(ib) then
-			  log(red(ts(bnamesrc).." not a valid iblock instance"))
-			  os.exit(1)
-		       end
-
-		       local btgt = ubx.block_get(ni, bnametgt)
-		       local ptgt = ubx.port_get(btgt, pnametgt)
-
-		       if ubx.port_connect_in(ptgt, ib) ~= 0 then
-			  log(red("failed to connect interaction "..bnamesrc..
-				  " to port "..ts(bnametgt).."."..ts(pnametgt)))
-		       end
-		       log("    "..green(bnamesrc).." (iblock) -> "..green(ts(bnametgt)).."."..cyan(ts(pnametgt)))
-
-		    elseif pnametgt==nil then
-		       -- src="block.port", tgt="iblock"
-		       local ib = ubx.block_get(ni, bnametgt)
-
-		       if ib==nil then log(red("unkown block "..ts(bnametgt))); os.exit(1) end
-
-		       if not ubx.is_iblock_instance(ib) then
-			  log(red(ts(bnametgt).." not a valid iblock instance"))
-			  os.exit(1)
-		       end
-
-		       local bsrc = ubx.block_get(ni, bnamesrc)
-		       local psrc = ubx.port_get(bsrc, pnamesrc)
-
-		       if ubx.port_connect_out(psrc, ib) ~= 0 then
-			  log(red("failed to connect "..ts(bnamesrc).."."..ts(pnamesrc)..
-				  " to "..ts(bnametgt)).." (iblock)")
-		       end
-		       log("    "..green(ts(bnamesrc)).."."..cyan(ts(pnamesrc)).." -> "..green(bnametgt).." (iblock)")
-		    else
-		       -- standard connection between two cblocks
-		       local bufflen = c.buffer_length or 1
-		       log("    "..green(ts(bnamesrc))..'.'..cyan(ts(pnamesrc)).." -["..
-			   yellow(ts(bufflen), true).."]".."-> "..green(ts(bnametgt)).."."..cyan(ts(pnametgt)))
-		       local bsrc = ubx.block_get(ni, bnamesrc)
-		       local btgt = ubx.block_get(ni, bnametgt)
-		       ubx.conn_lfds_cyclic(bsrc, pnamesrc, btgt, pnametgt, bufflen)
-		    end
-		 end, self.connections)
-
-   log("creating connections completed")
-
+   -- fire it up
+   local ni = ubx.node_create(t.nodename)
+   __launch(self, t, ni)
    return ni
 end
 
 -- exports
+M.is_system = is_system
 M.system = system
 M.system_spec = system_spec
+M.load = load
 
 return M
