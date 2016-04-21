5293696027c0c984fd5aa6cc77462a73ef28c75b
Hamish Guthrie <hamish.guthrie@kistler.com>
2016-04-21 14:41:24 +0200

    Include split function to remove dependency on utils.lua

--- tools/file2carr.lua
+++ tools/file2carr.lua
@@ -5,10 +5,27 @@
 --- data is \0 terminated.
 ---
 
-local utils=require "utils"
+function split(str, pat)
+   local t = {}  -- NOTE: use {n = 0} in Lua-5.0
+   local fpat = "(.-)" .. pat
+   local last_end = 1
+   local s, e, cap = str:find(fpat, 1)
+   while s do
+      if s ~= 1 or cap ~= "" then
+         table.insert(t,cap)
+      end
+      last_end = e+1
+      s, e, cap = str:find(fpat, last_end)
+   end
+   if last_end <= #str then
+      cap = str:sub(last_end)
+      table.insert(t, cap)
+   end
+   return t
+end
 
 function filename(path)
-   local t=utils.split(path, '/')
+   local t=split(path, '/')
    return t[#t]
 end
 
