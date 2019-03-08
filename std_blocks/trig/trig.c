e71a6c3725796e18959eb329f88e83b052427efa
Hamish Guthrie <hamish.guthrie@kistler.com>
2019-03-08 12:00:48 +0100

    Use microblx tstats

--- std_blocks/trig/trig.c
+++ std_blocks/trig/trig.c
@@ -18,8 +18,10 @@
 
 #include "types/trig_config.h"
 #include "types/trig_config.h.hexarr"
-#include "types/trig_tstat.h"
-#include "types/trig_tstat.h.hexarr"
+#include "../../std_types/stattypes/types/ubx_tstat.h"
+#include "../../std_types/stattypes/types/ubx_tstat.h.hexarr"
+
+#include "ubx_tstat.h"
 
 /* trig metadata */
 char trig_meta[] =
@@ -28,21 +30,31 @@ char trig_meta[] =
 	"}";
 
 ubx_port_t trig_ports[] = {
-	{ .name="tstats", .out_type_name="struct trig_tstat", .doc="timing statistics (if enabled)"},
+	{ .name="tstats", .out_type_name="struct ubx_tstat", .doc="timing statistics (if enabled)"},
 	{ NULL },
 };
 
 /* types defined by trig block */
 ubx_type_t trig_types[] = {
 	def_struct_type(struct trig_config, &trig_config_h),
-	def_struct_type(struct trig_tstat, &trig_tstat_h),
 	{ NULL },
 };
 
 /* configuration */
 ubx_config_t trig_config[] = {
 	{ .name="trig_blocks", .type_name = "struct trig_config", .doc="describes which blocks to trigger" },
-	{ .name="time_stats_enabled", .type_name = "int", .doc="set to 1 to enable min/max/avg trigger execution measurements" },
+	{ .name="profile_path", .type_name = "char" },
+	{ .name="tstats_enabled", .type_name = "int",
+	  .doc="enable timing statistics over all blocks",
+	},
+	{ .name="tstats_output_rate",
+	  .type_name = "unsigned int",
+	  .doc="output tstats only on every tstats_output_rate'th trigger (0 to disable)"
+	},
+	{ .name="tstats_print_on_stop",
+	  .type_name = "int",
+	  .doc="print tstats in stop()",
+	},
 	{ NULL },
 };
 
@@ -51,19 +63,36 @@ struct trig_inf {
 	struct trig_config *trig_list;
 	unsigned int trig_list_len;
 
-	struct trig_tstat tstats;
+	/* timing statistics */
+	const char *profile_path;
+	int tstats_enabled;
+	int tstats_print_on_stop;
+
+	struct ubx_tstat *tstats;
 
 	ubx_port_t *p_tstats;
 };
 
-def_write_fun(write_tstat, struct trig_tstat)
+def_write_fun(write_tstat, struct ubx_tstat)
 
-inline void tsnorm(struct timespec *ts)
+/**
+ * print tstats
+ */
+void trig_tstats_print(struct trig_inf *inf)
 {
-	if(ts->tv_nsec >= NSEC_PER_SEC) {
-		ts->tv_sec+=ts->tv_nsec / NSEC_PER_SEC;
-		ts->tv_nsec=ts->tv_nsec % NSEC_PER_SEC;
+	unsigned int i;
+
+	if (!inf->tstats_print_on_stop)
+		goto out;
+
+	if (inf->tstats_enabled) {
+		for (i = 0; i < inf->trig_list_len; i++)
+			if (inf->trig_list[i].measure)
+				tstat_print(inf->profile_path, &inf->tstats[i]);
 	}
+
+out:
+	return;
 }
 
 /* trigger the configured blocks */
@@ -71,30 +100,31 @@ int do_trigger(struct trig_inf *inf)
 {
 	int ret=-1;
 	unsigned int i, steps;
-	struct ubx_timespec ts_start, ts_end, ts_dur;
-
-	ubx_clock_mono_gettime(&ts_start);
+	struct ubx_timespec blk_ts_start, blk_ts_end;
 
 	for(i=0; i<inf->trig_list_len; i++) {
+		if(inf->trig_list[i].measure)
+			ubx_gettime(&blk_ts_start);
+
 		for(steps=0; steps<inf->trig_list[i].num_steps; steps++) {
 			if(ubx_cblock_step(inf->trig_list[i].b)!=0)
 				goto out;
+
+			/* per block statistics */
+			if(inf->trig_list[i].measure) {
+				ubx_gettime(&blk_ts_end);
+				tstat_update(&inf->tstats[i],
+					     &blk_ts_start,
+					     &blk_ts_end);
+			}
 		}
 	}
 
-	/* compute tstats */
-	ubx_clock_mono_gettime(&ts_end);
-	ubx_ts_sub(&ts_end, &ts_start, &ts_dur);
-
-	if(ubx_ts_cmp(&ts_dur, &inf->tstats.min) == -1)
-		inf->tstats.min=ts_dur;
-
-	if(ubx_ts_cmp(&ts_dur, &inf->tstats.max) == 1)
-		inf->tstats.max=ts_dur;
-
-	ubx_ts_add(&inf->tstats.total, &ts_dur, &inf->tstats.total);
-	inf->tstats.cnt++;
-	write_tstat(inf->p_tstats, &inf->tstats);
+	/* output stats - TODO throttling */
+	for(i=0; i<inf->trig_list_len; i++) {
+		if(inf->trig_list[i].measure)
+			write_tstat(inf->p_tstats, &inf->tstats[i]);
+	}
 
 	ret=0;
  out:
@@ -117,25 +147,12 @@ void trig_step(ubx_block_t *b)
 int trig_init(ubx_block_t *b)
 {
 	int ret = EOUTOFMEM;
-	struct trig_inf* inf;
 
 	if((b->private_data=calloc(1, sizeof(struct trig_inf)))==NULL) {
 		ERR("failed to alloc");
 		goto out;
 	}
 
-	inf=(struct trig_inf*) b->private_data;
-
-	inf->tstats.min.sec=999999999;
-	inf->tstats.min.nsec=999999999;
-
-	inf->tstats.max.sec=0;
-	inf->tstats.max.nsec=0;
-
-	inf->tstats.total.sec=0;
-	inf->tstats.total.nsec=0;
-
-	/* OK */
 	ret=0;
 	goto out;
 
@@ -147,8 +164,12 @@ int trig_start(ubx_block_t *b)
 {
 	DBG(" ");
 
+	int ret = -1;
 	struct trig_inf *inf;
 	ubx_data_t* trig_list_data;
+	long int len;
+	FILE *fp;
+	const int *val;
 
 	inf = (struct trig_inf*) b->private_data;
 
@@ -158,9 +179,62 @@ int trig_start(ubx_block_t *b)
 	inf->trig_list = trig_list_data->data;
 	inf->trig_list_len = trig_list_data->len;
 
+	/* this is freed in cleanup hook */
+	inf->tstats = realloc(
+		inf->tstats,
+		inf->trig_list_len * sizeof(struct ubx_tstat));
+
+	if(!inf->tstats) {
+		ERR("failed to alloc blk_stats");
+		goto out;
+	}
+
+	for(unsigned int i=0; i<inf->trig_list_len; i++) {
+		tstat_init(&inf->tstats[i],
+			   inf->trig_list[i].b->name);
+	}
+
+	if ((len = cfg_getptr_char(b, "profile_path", &inf->profile_path)) < 0) {
+		ERR("unable to retrieve profile_path parameter");
+		goto out;
+	}
+	/* truncate the file if it exists */
+	if (len > 0) {
+		fp = fopen(inf->profile_path, "w");
+		if (fp)
+			fclose(fp);
+	}
+
+	if((len = cfg_getptr_int(b, "tstats_print_on_stop", &val)) < 0)
+		goto out;
+
+	inf->tstats_print_on_stop = (len > 0) ? *val : 0;
+
+	if((len = cfg_getptr_int(b, "tstats_enabled", &val)) < 0)
+		goto out;
+
+	inf->tstats_enabled = (len > 0) ? *val : 0;
+
+	if(inf->tstats_enabled) {
+		DBG("tstats enabled");
+	}
+
 	inf->p_tstats = ubx_port_get(b, "tstats");
 
-	return 0;
+	ret = 0;
+
+out:
+	return ret;
+}
+
+void trig_stop(ubx_block_t *b)
+{
+	struct trig_inf *inf;
+	inf = (struct trig_inf *)b->private_data;
+
+	trig_tstats_print(inf);
+
+	free(inf->tstats);
 }
 
 void trig_cleanup(ubx_block_t *b)
@@ -179,6 +253,7 @@ ubx_block_t trig_comp = {
 
 	.init = trig_init,
 	.start = trig_start,
+	.stop = trig_stop,
 	.cleanup = trig_cleanup,
 	.step = trig_step
 };
