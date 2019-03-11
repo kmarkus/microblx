e4effff1d049d37f67614ceaed66022927ca0c89
Hamish Guthrie <hamish.guthrie@kistler.com>
2019-03-11 14:22:19 +0100

    Remove deprecated tstat includes, export ubx_tstat
    In case an external block wants to use timing statistics, we export the
    types/ubx_tstat.h include file.

--- std_blocks/ptrig/types/ptrig_tstat.h
+++
@@ -1,9 +0,0 @@
-struct ptrig_tstat
-{
-	char block_name[BLOCK_NAME_MAXLEN+1];
-	struct ubx_timespec min;
-	struct ubx_timespec max;
-	struct ubx_timespec total;
-	unsigned long cnt;
-};
-
--- std_blocks/trig/types/trig_tstat.h
+++
@@ -1,9 +0,0 @@
-
-struct trig_tstat
-{
-	struct ubx_timespec min;
-	struct ubx_timespec max;
-	struct ubx_timespec total;
-	unsigned long cnt;
-};
-
