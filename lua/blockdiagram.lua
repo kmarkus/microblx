b6eb9e5c60f9729b588dbd42063eeb7ac1984a79
Markus Klotzbuecher <mk@mkio.de>
2019-04-26 13:39:26 +0200

    blockdiagram: set default node name to 'n'
    For the many single node use-cases a longer node name is just overhead
    in the log messages, and for multi-node use-cases it can be set on the
    ubx_launch cmdline.
    
    Signed-off-by: Markus Klotzbuecher <mk@mkio.de>

--- lua/blockdiagram.lua
+++ lua/blockdiagram.lua
@@ -588,7 +588,7 @@ function system.launch(self, t)
 
    -- fire it up
    t = t or {}
-   t.nodename = t.nodename or "node-"..os.date("%Y%m%d_%H%M%S")
+   t.nodename = t.nodename or "n"
    verbose = t.verbose
 
    local ni = ubx.node_create(t.nodename, t.loglevel)
