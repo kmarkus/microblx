c88da6539fd405411b7ff0873129ff9e4fa12421
Hamish Guthrie <hamish.guthrie@kistler.com>
2019-05-08 09:04:04 +0200

    rtlog: add rtlog client library
    Signed-off-by: Hamish Guthrie <hamish.guthrie@kistler.com>

---
+++ libubx/rtlog_client.h
@@ -0,0 +1,16 @@
+/*
+ * rtlog_client.h: client library headers
+ *
+ * Copyright (C) 2018-2019 Markus Klotzbuecher <mk@mkio.de>
+ * Copyright (C) 2019 Hamish Guthrie <hamish.guthrie@kistler.com>
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+#ifndef RTLOG_CLIENT_H
+#define RTLOG_CLIENT_H
+#include "internal/rtlog_common.h"
+#include "internal/rtlog_client.h"
+
+#define LOG_BUFFER_DEPTH 1000
+#define LOG_SHM_FILENAME "rtlog.logshm"
+#endif /* RTLOG_CLIENT_H */
