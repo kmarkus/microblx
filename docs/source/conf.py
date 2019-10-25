b825f680cdd8327b687d7e82fc470d1887ab1034
Markus Klotzbuecher <mk@mkio.de>
2019-10-25 23:37:46 +0200

    doc: enable syntax highlighting
    Signed-off-by: Markus Klotzbuecher <mk@mkio.de>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,7 +71,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'default'
 
 
 # -- Options for HTML output -------------------------------------------------
