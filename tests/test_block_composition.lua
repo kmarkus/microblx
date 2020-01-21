01e8d1b618ce6e72d367a8065a9bf3d23c442db1
Markus Klotzbuecher <mk@mkio.de>
2020-01-21 22:55:07 +0100

    tests: blockdiagram: extend tests
    Signed-off-by: Markus Klotzbuecher <mk@mkio.de>

--- tests/test_block_composition.lua
+++ tests/test_block_composition.lua
@@ -19,8 +19,8 @@ local leaf = bd.system {
       { name = "trig", type="std_triggers/trig" },
    },
    configurations = {
-      {	name="rnd1", config = { min_max_config = { min=11, max=22 } } },
-      {	name="rnd2", config = { min_max_config = { min=111, max=222 } } },
+      {	name="rnd1", config = { min_max_config = { min=1, max=2 } } },
+      {	name="rnd2", config = { min_max_config = { min=3, max=4 } } },
       { name="trig", config = { trig_blocks={
 				   { b="#rnd1", num_steps=1, measure=0 },
 				   { b="#rnd2", num_steps=1, measure=0 } } } }
@@ -42,9 +42,9 @@ local comp1 = bd.system {
       { name = "trig", type="std_triggers/trig" },
    },
    configurations = {
-      {	name="rnd1", config = { min_max_config = { min=33, max=44 } } },
-      {	name="rnd2", config = { min_max_config = { min=444, max=555 } } },
-      {	name="leaf/rnd2", config = { min_max_config = { min=89, max=99 } } },
+      {	name="rnd1", config = { min_max_config = { min=10, max=11 } } },
+      {	name="rnd2", config = { min_max_config = { min=12, max=13 } } },
+      {	name="leaf/rnd2", config = { min_max_config = { min=14, max=15 } } },
       { name="trig", config = { trig_blocks={
 				   { b="#rnd1", num_steps=1, measure=0 },
 				   { b="#rnd2", num_steps=1, measure=0 },
@@ -69,9 +69,10 @@ local comp2 = bd.system {
       { name = "trig", type="std_triggers/trig" },
    },
    configurations = {
-      {	name="rnd1", config = { min_max_config = { min=33, max=44 } } },
-      {	name="rnd2", config = { min_max_config = { min=444, max=555 } } },
-      {	name="mid/leaf/rnd2", config = { min_max_config = { min=89, max=99 } } },
+      {	name="rnd1", config = { min_max_config = { min=100, max=101 } } },
+      {	name="rnd2", config = { min_max_config = { min=102, max=103 } } },
+      {	name="mid/rnd2", config = { min_max_config = { min=104, max=105 } } },
+      {	name="mid/leaf/rnd2", config = { min_max_config = { min=106, max=107 } } },
       { name="trig", config = { trig_blocks={
 				   { b="#rnd1", num_steps=1, measure=0 },
 				   { b="#rnd2", num_steps=1, measure=0 },
@@ -102,6 +103,11 @@ function TestComp:test_leaf()
    lu.assert_equals(num_err, 0)
    NI =	leaf:launch({ nodename="test_leaf", nostart=true})
    lu.assert_not_nil(NI)
+
+   -- check configs
+   lu.assert_equals( NI:b("rnd1"):c("min_max_config"):tolua(), { min=1, max=2 } )
+   lu.assert_equals( NI:b("rnd2"):c("min_max_config"):tolua(), { min=3, max=4 } )
+
 end
 
 function TestComp:test_comp1()
@@ -111,18 +117,10 @@ function TestComp:test_comp1()
    lu.assert_not_nil(NI)
 
    -- check configs
-   lu.assert_equals(
-      NI:b("rnd1"):c("min_max_config"):tolua(), { min=33, max=44 })
-
-   lu.assert_equals(
-      NI:b("rnd2"):c("min_max_config"):tolua(), { min=444, max=555 })
-
-   lu.assert_equals(
-      NI:b("leaf/rnd1"):c("min_max_config"):tolua(), { min=11, max=22 })
-
-   lu.assert_equals(
-      NI:b("leaf/rnd2"):c("min_max_config"):tolua(), { min=89, max=99 })
-
+   lu.assert_equals( NI:b("rnd1"):c("min_max_config"):tolua(), { min=10, max=11 } )
+   lu.assert_equals( NI:b("rnd2"):c("min_max_config"):tolua(), { min=12, max=13 } )
+   lu.assert_equals( NI:b("leaf/rnd1"):c("min_max_config"):tolua(), { min=1, max=2 } )
+   lu.assert_equals( NI:b("leaf/rnd2"):c("min_max_config"):tolua(), { min=14, max=15 } )
 end
 
 function TestComp:test_comp2()
@@ -130,6 +128,35 @@ function TestComp:test_comp2()
    lu.assert_equals(num_err, 0)
    NI =	comp2:launch({ nodename="test_comp2", nostart=true})
    lu.assert_not_nil(NI)
+
+   lu.assert_equals( NI:b("rnd1"):c("min_max_config"):tolua(), { min=100, max=101 } )
+   lu.assert_equals( NI:b("rnd2"):c("min_max_config"):tolua(), { min=102, max=103 } )
+   lu.assert_equals( NI:b("mid/rnd1"):c("min_max_config"):tolua(), { min=10, max=11 } )
+   lu.assert_equals( NI:b("mid/rnd2"):c("min_max_config"):tolua(), { min=104, max=105 } )
+   lu.assert_equals( NI:b("mid/leaf/rnd1"):c("min_max_config"):tolua(), { min=1, max=2 } )
+   lu.assert_equals( NI:b("mid/leaf/rnd2"):c("min_max_config"):tolua(), { min=106, max=107 } )
+end
+
+local leaf = bd.system {
+   imports = { "stdtypes", "random", "lfds_cyclic", "trig" },
+   blocks = {
+      { name = "rnd1", type="random/random" },
+      { name = "rnd2", type="random/random" },
+      { name = "trig", type="std_triggers/trig" },
+   },
+   configurations = {
+      {	name="rnd1", config = { min_max_config = { min=1, max=2 } } },
+      {	name="rnd2", config = { min_max_config = { min=3, max=4 } } },
+      { name="trig", config = { trig_blocks={
+				   { b="#rnd1", num_steps=1, measure=0 },
+				   { b="#rnd2", num_steps=1, measure=0 } } } }
+   },
+   connections = {
+      { src="rnd1.rnd", tgt="rnd2.seed" }
+   },
+}
+
+function TestComp:test_nodecfg1()
 end
 
 -- TODO
