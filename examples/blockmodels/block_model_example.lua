888759323c3e1000fc386b3e9d06d3d1305a3045
Markus Klotzbuecher <mk@mkio.de>
2020-06-10 16:36:43 +0200

    ubx-genblock: update and cleanup
     - update accessor generation
     - add min/max support
     - generate include guards for types
     - general Lua improvements (local, ...)
    
    Signed-off-by: Markus Klotzbuecher <mk@mkio.de>

--- examples/blockmodels/block_model_example.lua
+++ examples/blockmodels/block_model_example.lua
@@ -1,24 +1,27 @@
 return block 
 {
-      name="myblock",
-      license="MIT",
-      meta_data="",
-      port_cache=true,
+      name = "myblock",
+      license = "MIT",
+      meta_data = "",
 
+      -- generate code for caching port poitners:
+      port_cache = true,
+
+      -- generate stubs and support for the following struct types:
       types = {
-	 { name="vector", class='struct' }, -- Enum will follow once implemented in C
-	 { name="robot_data", class='struct' },
+	 { name = "vector", class='struct' },
+	 { name = "robot_data", class='struct' },
       },
 
-      configurations= {
-	 { name="joint_limits", type_name="double", len=5 },
-	 { name="info", type_name="struct robot_data" },
+      configurations = {
+	 { name = "joint_limits", type_name = "double", min=0, max=6 },
+	 { name = "info", type_name = "struct robot_data" },
       },
 
       ports = {
-	 { name="pos", in_type_name="double", in_data_len=5, doc="measured position [m]" },
-	 { name="ctrl_mode", in_type_name="int32_t", out_type_name="int32_t" },
+	 { name = "pos", in_type_name = "double", in_data_len = 5, doc="measured position [m]" },
+	 { name = "ctrl_mode", in_type_name = "int32_t", out_type_name = "int32_t" },
       },
-      
+
       operations = { start=true, stop=true, step=true }
 }
