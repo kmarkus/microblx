4197123c9f949e81de87fbb78048ee162d9a9113
Markus Klotzbuecher <mk@mkio.de>
2026-09-10 10:43:55 +0200

    libubx: add ubx_nanosleep_hybrid
    Signed-off-by: Markus Klotzbuecher <mk@mkio.de>

--- libubx/ubx_time.h
+++ libubx/ubx_time.h
@@ -6,6 +6,7 @@
 int ubx_gettime(struct ubx_timespec *uts);
 int ubx_nanosleep(const struct ubx_timespec *dur);
 int ubx_nanowait(const struct ubx_timespec *dur);
+int ubx_nanosleep_hybrid(const struct ubx_timespec *dur, uint64_t slack_ns);
 int ubx_ts_cmp(const struct ubx_timespec *ts1, const struct ubx_timespec *ts2);
 void ubx_ts_norm(struct ubx_timespec *ts);
 void ubx_ts_sub(const struct ubx_timespec *ts1, const struct ubx_timespec *ts2, struct ubx_timespec *out);
